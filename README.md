cb5a4e5904d82cc77b35d5b058884c918b6aa443
mrava87 <matteoravasi@gmail.com>
2019-01-16 21:40:58 +0100

    Finished script to make automatic plots

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## PyFFTW Playground
-This repo is used to identify if PyFFTW can be included in Pylops and create a .travis pipeline for installing
-and using PyFFTW
+This repo contains recipes to install PyFFTW, and a python script to test the performnce of
+different ffts (numpy, scipy, FFTW) in pythob
 
-### Installation steps on mac
 
+### Installation steps
 Create environment:
 ```
 make install_conda
@@ -20,3 +20,18 @@ Install pyfftw:
     ```
     conda install -c conda-forge pyfftw
     ```
+
+### Benchmarking
+
+A python script called ``benchmark.py`` can be used to benchmark the speed of different ffts (numpy, scipy, FFTW) in your system.
+Run the script in the following way
+
+```
+python performance.py computer fft 10
+```
+where you can specify your computer name (eg ``mac_book_air_1_3_GHz_Intel_Core_i5``), if you want to use
+numpy fft (``fft``) or fftpack (``fftpack``) - the first using MKL if installed with conda, the second being the slower if MKL
+is not available - and the number of times an fft is tested by timeit function (>10 to be somehow reliable).
+
+This script will automatically create some summary plots, please commit them so with time we will
+get a library of performance tests and can see which fft is best in which environment.
--- benchmark.py
+++ benchmark.py
@@ -1,13 +1,34 @@
-# Example modified from https://github.com/keflavich/agpy/blob/master/tests/test_ffts.py
+# Benchmark performance (modified from https://github.com/keflavich/agpy/blob/master/tests/test_ffts.py)
+#
+# Usage: python performance.py computer fft 10
+#
+# where you can specify your computer (eg mac_book_air_1_3_GHz_Intel_Core_i5), if you want to use
+# np.fft or fftpack (the first using MKL if installed with conda, the second being the slower if MKL
+# is not available), and the number of times an fft is tested by timeit function (>10 to be somehow
+# reliable)
+#
+# Author: Matteo Ravasi
+#
+########################################################################################################
+import sys
 import timeit
 import os
+import matplotlib.pyplot as plt
 import scipy.fftpack
 spfftn = scipy.fftpack.fft
 spifftn = scipy.fftpack.ifft
 
 import numpy as np
-npfftn = np.fft.fft
-npifftn = np.fft.ifft
+if sys.argv[2]=='fft':
+	print('use np.fft')
+	nplabel='np_fft'
+	npfftn = np.fft.fft
+	npifftn = np.fft.ifft
+elif sys.argv[2]=='fftpack':
+	print('use np.fft.fftpack')
+	nplabel='np_fftpack'
+	npfftn = np.fft.fftpack.fft
+	npifftn = np.fft.fftpack.ifft
 
 import pyfftw
 def wfftn(f):
@@ -22,13 +43,12 @@ def wifftn(f):
 if __name__ == "__main__":
 	print('OMP_NUM_THREADS', os.environ.get('OMP_NUM_THREADS'))
 	print('MKL_NUM_THREADS', os.environ.get('MKL_NUM_THREADS'))
-	
-	# check results
+
+	# check obtain same results
 	for dir  in ['', 'i']:
 		print('Forward' if dir=='' else 'Inverse')
 		for ndims in [1,2,3]:
-			for ii in range(2, 15-ndims*2):
-
+			for ii in range(2, 15 - ndims * 2):
 				array = np.random.random([2**ii]*ndims)
 				outnp=npfftn(array, axis=-1)
 				outsp=spfftn(array, axis=-1)
@@ -38,31 +58,51 @@ if __name__ == "__main__":
 				fft_forward = pyfftw.FFTW(array,outarray, axes=(-1,), direction='FFTW_FORWARD', flags=['FFTW_ESTIMATE'], threads=4)
 				outw=wfftn(fft_forward)
 
-				print("\n%i-dimensional arrays" % ndims)
-				print('np-fftw',np.linalg.norm(outnp-outw))
-				print('sp-fftw',np.linalg.norm(outsp-outw))
+				#print("\n%i-dimensional arrays" % ndims)
+				#print('np-fftw',np.linalg.norm(outnp-outw))
+				#print('sp-fftw',np.linalg.norm(outsp-outw))
+				if np.linalg.norm(outnp-outw)>1e-10 or np.linalg.norm(outsp-outw)>1e-10:
+					raise ValueError('np-sp-fftw do not give same results for %s %d dim % ii size' %(dir, ndims, ii))
 
-	# check speed
+	# compare speed
+	fig, axs = plt.subplots(3, 1, figsize=(15, 12))
+	fig.suptitle(sys.argv[1]+'(OMP_NUM_THREADS=%s, MKL_NUM_THREADS=%s)'
+				 %(os.environ.get('OMP_NUM_THREADS'), os.environ.get('MKL_NUM_THREADS')))
+	axs[0].set_title('1d signal')
+	axs[1].set_title('2d signal')
+	axs[2].set_title('3d signal')
 	for dir  in ['', 'i']:
 		print('Forward' if dir=='' else 'Inverse')
 		for ndims in [1,2,3]:
 			print("\n%i-dimensional arrays" % ndims)
 			print(" ".join(["%17s" % n for n in ("n","sp","np","fftw")]))
-		
-			for ii in range(2, 15-ndims*2):
-				
-				setup="import example; import numpy as np; array = np.random.random([%i]*%i)" % (2**ii,ndims)
-				setup1="import example; import numpy as np; import pyfftw;"\
+			sizes = range(2, 15 - ndims * 2)
+			np_speed = np.zeros(len(sizes))
+			sp_speed = np.zeros(len(sizes))
+			fftw_speed = np.zeros(len(sizes))
+			for i, ii in enumerate(sizes):
+				setup="import benchmark; import numpy as np; array = np.random.random([%i]*%i)" % (2**ii,ndims)
+				setup1="import benchmark; import numpy as np; import pyfftw;"\
 					  "array = np.random.random([%i]*%i);"\
 					  "array = array.astype('complex'); outarray = array.copy();"\
 					  "fft_forward = pyfftw.FFTW(array,outarray, direction='FFTW_FORWARD', flags=['FFTW_ESTIMATE'], threads=4)" % (2**ii,ndims)
-			
+
+				tnp = min(timeit.Timer(stmt="benchmark.%s%sfftn(array)" % ('np', dir), setup=setup).repeat(3,int(sys.argv[3])))
+				tsp = min(timeit.Timer(stmt="benchmark.%s%sfftn(array)" % ('sp', dir), setup=setup).repeat(3, int(sys.argv[3])))
+				tfftw = min(timeit.Timer(stmt="benchmark.%s%sfftn(fft_forward)" % ('w', dir), setup=setup1).repeat(3, int(sys.argv[3])))
+				np_speed[i] = tnp
+				sp_speed[i] = tsp
+				fftw_speed[i] = tfftw
 				print("%16i:" % (int(2**ii)) + \
-						"".join(
-							["%17f" % (min(timeit.Timer(stmt="example.%s%sfftn(array)" % (ffttype, dir), setup=setup).repeat(3,10))) for ffttype in ('sp','np')] +
-							["%17f" % (min(timeit.Timer(stmt="example.%s%sfftn(fft_forward)" % (ffttype, dir), setup=setup1).repeat(3,10))) for ffttype in ('w')]
-						))
-                
+						"".join(["%17f" % (i) for i in [tsp, tnp, tfftw]]))
+			axs[ndims-1].semilogy(np_speed, 'r' if dir=='' else '--r', label=nplabel if dir=='' else nplabel+'-inv')
+			axs[ndims-1].semilogy(sp_speed, 'g' if dir=='' else '--g', label='sp' if dir=='' else 'sp-inv')
+			axs[ndims-1].semilogy(fftw_speed, 'b' if dir=='' else '--b', label='fftw' if dir=='' else 'fftw-inv')
+	axs[0].legend(loc=2)
+	plt.savefig(os.path.join('benchmarks', sys.argv[1]+'__'+nplabel+'.png'), dpi=300)
+	plt.show()
+
+
 """
 RESULTS: (on mac book air 1.3 GHz Intel Core i5) - using np.fft.fftpack
 OMP_NUM_THREADS 1
